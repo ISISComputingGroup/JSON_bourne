fcfc6029f7ecb432245b4e809801e4395053bb6d
Jack Draper <drapj002@gmail.com>
2017-08-02 17:28:54 +0100

    Added iframe for jenkins wall display.

--- ibexOverview.js
+++ ibexOverview.js
@@ -45,6 +45,31 @@ function close_window(){
 	
 }
 
+function open_wall_display(){
+	clearBox("new_window")
+	var newWindow = document.createElement("div");
+	var newWindowStyle = document.createAttribute("class");
+	newWindowStyle.value = "col-sm-12 well";
+	newWindow.setAttributeNode(newWindowStyle);
+	newWindow.id = "internal"
+	document.getElementById("new_window").appendChild(newWindow);
+	var closeButton = document.createElement("button");
+	closeButton.innerHTML = "x";
+	var blockListStyle = document.createAttribute("onclick");
+    blockListStyle.value = "close_window()";
+	closeButton.setAttributeNode(blockListStyle);
+	document.getElementById("internal").appendChild(closeButton);
+	var newIframe = document.createElement("iframe");
+	newIframe.src = "http://epics-jenkins.isis.rl.ac.uk/plugin/jenkinswalldisplay/walldisplay.html?viewName=All&jenkinsUrl=http%3A%2F%2Fepics-jenkins.isis.rl.ac.uk%2F"
+	newIframe.height = "540px"
+	newIframe.width = "100%"
+	newIframe.frameborder = "0"
+	document.getElementById("internal").appendChild(newIframe);
+	
+	
+}
+
+
 function on_click(elmnt) {
 	clearBox("new_window")
 	var newWindow = document.createElement("div");
