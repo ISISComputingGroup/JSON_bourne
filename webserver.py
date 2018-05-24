27d8c48d825451ad9265e20d50106c12efbc97af
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-05-24 12:08:34 +0100

    Add MAPS to instrument list

--- webserver.py
+++ webserver.py
@@ -21,7 +21,7 @@ HOST, PORT = '', 60000
 ALL_INSTS = {"MUONFE": "NDEMUONFE"}  # Used for non NDX hosts format of {name: host}
 
 NDX_INSTS = ["DEMO", "LARMOR", "IMAT", "IRIS", "VESUVIO", "ALF", "ZOOM", "POLARIS", "HRPD", "MERLIN", "ENGINX",
-             "RIKENFE", "EMMA-A", "SANDALS", "GEM"]
+             "RIKENFE", "EMMA-A", "SANDALS", "GEM", "MAPS"]
 
 for inst in NDX_INSTS:
     ALL_INSTS[inst] = "NDX" + inst
