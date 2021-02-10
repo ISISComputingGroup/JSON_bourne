e4bdcc3039e3f323dd15606af0bdee9aabb8c614
Mihai Stavila <mihai.stavila@stfc.ac.uk>
2021-02-10 12:17:58 +0000

    Hide empty ("N/A") blocks and unneeded EMU alarms

--- front_end/display_blocks.js
+++ front_end/display_blocks.js
@@ -53,6 +53,9 @@ dictLongerInstPVs = {
     "BANNER:LEFT:LABEL"   : "BANNER:LEFT:VALUE",
 }
 
+instHiddenAlarms = {
+    "emu" : [dictInstPV.BEAMCURRENT, dictInstPV.TOTALUAMPS, dictInstPV.SIM_MODE]
+}
 
 /**
  * Gets the proper display title for a PV.
@@ -377,9 +380,13 @@ function displayOneBlock(node, block, blockName, linkGraph) {
         if (rc_enabled === "YES" && (rc_inrange === "YES" || rc_inrange === "NO")) {
             writeRangeInfo(nodeBlock, rc_inrange);
         }
-        // write alarm status if active
-        if (!alarm.startsWith("null") && alarm !== "") {
-            writeAlarmInfo(nodeBlock, alarm);
+        
+        // if instrument does not have alarms set to hidden for this block
+        if (typeof instHiddenAlarms[instrument] === "undefined" || !instHiddenAlarms[instrument].includes(blockName)) {
+            // write alarm status if active
+            if (!alarm.startsWith("null") && alarm !== "") {
+                writeAlarmInfo(nodeBlock, alarm);
+            }
         }
     }
     node.appendChild(nodeBlock);
@@ -398,8 +405,10 @@ function getDisplayBlocks(node, blocks, linkGraph) {
     for (var key in blocks) {
         if (key in dictLongerInstPVs) {
             var block = blocks[key];
-            var label = block["value"] == "" ? "N/A" : block["value"].slice(0,-1);
-            displayOneBlock(node, blocks[dictLongerInstPVs[key]], label, linkGraph);
+            if (block["value"] != "") {
+                var label = block["value"].slice(0, -1);
+                displayOneBlock(node, blocks[dictLongerInstPVs[key]], label, linkGraph);
+            }  
         } else if (ignore_pvs.indexOf(key) >= 0) {
             // Do nothing
         } else {
